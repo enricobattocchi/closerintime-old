e74f2b6bfa9cf8718218773218e653dfd6c650ef
lopo <lopo@lopo-nettop>
2017-02-09 00:35:37 +0100

    ?

--- js/suggestions-sync.js
+++ js/suggestions-sync.js
@@ -1,8 +1,26 @@
 self.addEventListener('sync', function(event) {
   if (event.tag == 'suggestions') {
-    event.waitUntil(function(){
-    	console.log("Performing a background sync");
-    	pushSuggestions();    		
-    });
+    event.waitUntil(pushSuggestionsSW());
   }
 });
+
+
+function pushSuggestionsSW(){
+	db = new Dexie("closerintime");
+	
+	db.suggestions
+	.toArray()
+	.then(function(data){
+     	$.ajax({
+     		method: 'post',
+            url: 'suggest.php',
+            data: {arr: JSON.stringify(data)},
+            success: function(data, status) {
+                console.log(data);
+                if(data == 1){
+                	db.suggestions.clear();
+                }
+            }
+        });
+	});
+}
