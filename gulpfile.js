790b1b2192e16edcefc0fe9daaba8108f4aaebb2
lopo <lopo@lopo-nettop>
2017-02-11 02:45:07 +0100

    sw generation moved to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 
+var packageJson = require('./package.json');
+
 
 //style paths
 var sassFiles = 'css/*.scss',  
     cssDest = 'css/';
 
-gulp.task('default', ['style']);
+gulp.task('default', ['style', 'generate-service-worker']);
 
 gulp.task('style', function(){  
     gulp.src(sassFiles)
@@ -16,3 +18,30 @@ gulp.task('style', function(){
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest(cssDest));
 });
+
+
+
+gulp.task('generate-service-worker', function(callback) {
+	var path = require('path');
+	var swPrecache = require('sw-precache');
+
+	swPrecache.write('service-worker.js',{
+		cacheId: packageJson.name,
+		staticFileGlobs: [
+		                  'css/**.css',
+		                  '**.html',
+		                  'img/**.*',
+		                  'js/**.js',
+		                  'fonts/**.*'
+		                  ],
+        importScripts: [
+                        'js/dexie.min.js',
+                        'js/suggestions-sync.js'
+                        ],
+        verbose: true,
+        runtimeCaching: [{
+        				urlPattern: 'lookup.php',
+    					handler: 'networkFirst'
+        				}]
+		}, callback);
+	});
