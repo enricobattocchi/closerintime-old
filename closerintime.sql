e2e82ee8d2d975f1cda14e04e9b309e23888d0ab
lopo <lopo@lopo-nettop>
2017-02-22 19:58:10 +0100

    added media category

--- closerintime.sql
+++ closerintime.sql
@@ -1,4 +1,3 @@
-
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE `events` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -6,7 +5,7 @@ CREATE TABLE `events` (
   `year` smallint(6) NOT NULL,
   `month` tinyint(3) unsigned DEFAULT NULL,
   `day` tinyint(3) unsigned DEFAULT NULL,
-  `type` enum('music','film','building','book','history','science','art','computer','sport','submitted') NOT NULL,
+  `type` enum('music','film','building','book','history','science','art','computer','sport','media','submitted') NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT '1',
   `plural` tinyint(1) NOT NULL DEFAULT '0',
   `link` varchar(255) DEFAULT NULL,
@@ -50,3 +49,4 @@ CREATE TABLE `verification_view` (`uuid` varchar(40), `source_uuid` varchar(40))
 
 DROP TABLE IF EXISTS `verification_view`;
 CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `verification_view` AS select `e`.`uuid` AS `uuid`,`s`.`source_uuid` AS `source_uuid` from (`events` `e` left join `substitutions` `s` on((`e`.`uuid` = `s`.`target_uuid`)));
+
