d9ceeabc45e4ddd92854769dd0c1b9e22cd25541
lopo <lopo@lopo-nettop>
2017-02-24 09:39:07 +0100

    no unique in events since we don't need them

--- js/script.js
+++ js/script.js
@@ -562,6 +562,11 @@ function initIndexedDB(){
 		localevents: "++id, &name, year, month, day, type, plural, sent, link, &uuid"
 	});
 
+	db.version(17).stores({
+		events: "id, name, year, month, day, type, plural, enabled, link, uuid",
+		localevents: "++id, &name, year, month, day, type, plural, sent, link, &uuid"
+	});
+	
 
 	db.on('ready', function () {
 		return db.events.clear()
