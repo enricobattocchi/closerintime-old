fc62956636405a6b4849e70163363a3aceb05470
lopo <lopo@lopo-nettop>
2017-02-27 22:13:38 +0100

    store IP address in suggestions

--- suggest.php
+++ suggest.php
@@ -21,11 +21,12 @@ if(count($array) > 0){
 		$day = $suggestion->day ? $db->real_escape_string($suggestion->day) : 'NULL';
 		$uuid = $suggestion->uuid ? $db->real_escape_string($suggestion->uuid) : 'NULL';
 		$type = $suggestion->type ? $db->real_escape_string($suggestion->type) : 'NULL';
+		$ip = inet_pton(determineIP());
 	
 		
-		$suggestions = "( '$name', $year, $month, $day, '$type', '0', '', '$uuid' )";
+		$suggestions = "( '$name', $year, $month, $day, '$type', '0', '', '$uuid', '$ip' )";
 		
-		$sql = "INSERT INTO suggestions (`name`, `year`, `month`, `day`, `type`, `enabled`, `link`, `uuid`) VALUES " . $suggestions;
+		$sql = "INSERT INTO suggestions (`name`, `year`, `month`, `day`, `type`, `enabled`, `link`, `uuid`, `ip`) VALUES " . $suggestions;
 		
 		$res = $db->query( $sql );
 		
@@ -36,3 +37,35 @@ if(count($array) > 0){
 }
 
 echo json_encode($result);
+
+
+function checkIP($ip) {
+	if (!empty($ip) && inet_pton($ip) != false) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+function determineIP() {
+	if (checkIP($_SERVER["HTTP_CLIENT_IP"])) {
+		return $_SERVER["HTTP_CLIENT_IP"];
+	}
+	foreach (explode(",",$_SERVER["HTTP_X_FORWARDED_FOR"]) as $ip) {
+		if (checkIP(trim($ip))) {
+			return $ip;
+		}
+	}
+	if (checkIP($_SERVER["HTTP_X_FORWARDED"])) {
+		return $_SERVER["HTTP_X_FORWARDED"];
+	} elseif (checkIP($_SERVER["HTTP_X_CLUSTER_CLIENT_IP"])) {
+		return $_SERVER["HTTP_X_CLUSTER_CLIENT_IP"];
+	} elseif (checkIP($_SERVER["HTTP_FORWARDED_FOR"])) {
+		return $_SERVER["HTTP_FORWARDED_FOR"];
+	} elseif (checkIP($_SERVER["HTTP_FORWARDED"])) {
+		return $_SERVER["HTTP_FORWARDED"];
+	} else {
+		return $_SERVER["REMOTE_ADDR"];
+	}
+}
