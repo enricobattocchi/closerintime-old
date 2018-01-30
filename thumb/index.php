0d85e1410e62d68e43d92e6ebdeb9f545b53d573
lopo <enrico.battocchi@gmail.com>
2018-01-30 22:08:03 +0100

    Create sharing image for 3 points

--- thumb/index.php
+++ thumb/index.php
@@ -1,26 +1,48 @@
 <?php
 $a = is_numeric($_GET['a']) ? $_GET['a'] : false;
 $b = is_numeric($_GET['b']) ? $_GET['b'] : false;
+$c = is_numeric($_GET['c']) ? $_GET['c'] : false;
 
 $im = new Imagick('sharing-img-template.png');
 
-if($a && $b && ($src1 = new Imagick("$a.png")) && ($src2 = new Imagick("$b.png")) ){
+if($a && $b && $c && ($src1 = new Imagick("$a.png")) && ($src2 = new Imagick("$b.png")) && ($src3 = new Imagick("$c.png"))){
+	$im = new Imagick('sharing-img-template-3.png');
+	/* Append the images into one */
+	//$im->resetIterator();
+	//$combined = $im->appendImages(false);
 	
-		/* Append the images into one */
-		//$im->resetIterator();
-		//$combined = $im->appendImages(false);
-		
-		$src1->resizeImage(370, 370, Imagick::FILTER_LANCZOS, 1);
-		$src2->resizeImage(370, 370, Imagick::FILTER_LANCZOS, 1);
+	$src1->resizeImage(270, 270, Imagick::FILTER_LANCZOS, 1);
+	$src2->resizeImage(270, 270, Imagick::FILTER_LANCZOS, 1);
+	$src3->resizeImage(270, 270, Imagick::FILTER_LANCZOS, 1);
 	
-		$im->compositeImage($src1, Imagick::COMPOSITE_DEFAULT, 116, 150);
-		$im->compositeImage($src2, Imagick::COMPOSITE_DEFAULT, 716, 150);
-		
-		/* Output the image */
-		$im->setImageFormat("png");
-		$im->writeImage ($a.'_'.$b.'.png');
-		header("Content-Type: image/png");
-		echo $im;
+	$im->compositeImage($src1, Imagick::COMPOSITE_DEFAULT, 65, 202);
+	$im->compositeImage($src2, Imagick::COMPOSITE_DEFAULT, 465, 202);
+	$im->compositeImage($src3, Imagick::COMPOSITE_DEFAULT, 865, 202);
+	
+	/* Output the image */
+	$im->setImageFormat("png");
+	$im->writeImage ($a.'_'.$b.'_'.$c.'.png');
+	header("Content-Type: image/png");
+	echo $im;
+	
+	
+} else if($a && $b && ($src1 = new Imagick("$a.png")) && ($src2 = new Imagick("$b.png")) ){	
+	$im = new Imagick('sharing-img-template.png');
+	/* Append the images into one */
+	//$im->resetIterator();
+	//$combined = $im->appendImages(false);
+	
+	$src1->resizeImage(370, 370, Imagick::FILTER_LANCZOS, 1);
+	$src2->resizeImage(370, 370, Imagick::FILTER_LANCZOS, 1);
+
+	$im->compositeImage($src1, Imagick::COMPOSITE_DEFAULT, 116, 150);
+	$im->compositeImage($src2, Imagick::COMPOSITE_DEFAULT, 716, 150);
+	
+	/* Output the image */
+	$im->setImageFormat("png");
+	$im->writeImage ($a.'_'.$b.'.png');
+	header("Content-Type: image/png");
+	echo $im;
 
 } else {
 	die('Error');
